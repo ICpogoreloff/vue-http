8a01887476c36187b0568831367f490b2345d63b
ICpogoreloff <icupayliedie@gmail.com>
2022-02-20 18:37:32 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <form action="" class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+    <div class="card">
+      <label for="name">Введите имя: </label>
+      <input type="text" id="name" v-model.trim="name">
+    </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    createPerson() {
+      // this.name
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
